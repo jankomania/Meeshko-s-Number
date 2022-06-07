12bc39c127d35ac2d17288f91da491fc492690de
jankomania <jankomania.co@gmail.com>
2022-06-07 17:21:28 +0200

    Improved functionality
    Deleted some unnecessary code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 version = 0.1
 
+import time
 import kivy
 from kivy.app import App
 from kivy.lang import Builder
@@ -8,14 +9,13 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
 
 class Game():
-    def __init__(self, difficulty='Medium', lives='10', level='0', score='0', label_from='0', label_to='0', label_input='') -> None:
+    def __init__(self, difficulty='Medium', lives='10', level='0', score='0', label_from='0', label_to='0') -> None:
         self.difficulty = difficulty
         self.lives = lives
         self.level = level
         self.score = score
         self.label_from = label_from
         self.label_to = label_to
-        self.label_input = label_input
 
     def printr(self):
         print(self.difficulty)
@@ -24,7 +24,6 @@ class Game():
         print(self.score)
         print(self.label_from)
         print(self.label_to)
-        print(self.label_input)
 
 
 game = Game()
@@ -33,114 +32,108 @@ game = Game()
 class MainMenu(Screen):
     def easy(self):
         game.difficulty = 'Easy'
-        game.lives = 20
+        game.lives = '20'
 
     def med(self):
         game.difficulty = 'Medium'
-        game.lives = 10
+        game.lives = '10'
 
 
     def hard(self):
         game.difficulty = 'Hard'
-        game.lives = 5
+        game.lives = '5'
 
     def printr(self):
         game.printr()
 
     def start_game(self):
-        if game.difficulty == 'Easy': game.lives = 20
-        elif game.difficulty == 'Medium': game.lives = 10
-        else: game.lives == 5
+        if game.difficulty == 'Easy': game.lives = '20'
+        elif game.difficulty == 'Medium': game.lives = '10'
+        else: game.lives == '5'
 
 
 class GameWin(Screen):
     label_input = ObjectProperty(None)
+    label_lives = ObjectProperty(None)
+
+    def update_lives(self):
+        self.label_lives.text = game.lives
+
 
     def one(self):
         if len(self.label_input.text) > 7: pass
         elif self.label_input.text == '':
             self.label_input.text = '1'
         else: self.label_input.text += '1'
-
-        game.label_input = self.label_input.text
+        
         
     def two(self):
         if len(self.label_input.text) > 7: pass
         elif self.label_input.text == '':
             self.label_input.text = '2'
         else: self.label_input.text += '2'
-
-        game.label_input = self.label_input.text
+        
         
     def three(self):
         if len(self.label_input.text) > 7: pass
         elif self.label_input.text == '':
             self.label_input.text = '3'
         else: self.label_input.text += '3'
-
-        game.label_input = self.label_input.text
+        
         
     def four(self):
         if len(self.label_input.text) > 7: pass
         elif self.label_input.text == '':
             self.label_input.text = '4'
         else: self.label_input.text += '4'
-
-        game.label_input = self.label_input.text
+        
         
     def five(self):
         if len(self.label_input.text) > 7: pass
         elif self.label_input.text == '':
             self.label_input.text = '5'
         else: self.label_input.text += '5'
-
-        game.label_input = self.label_input.text
+        
         
     def six(self):
         if len(self.label_input.text) > 7: pass
         elif self.label_input.text == '':
             self.label_input.text = '6'
         else: self.label_input.text += '6'
-
-        game.label_input = self.label_input.text
+        
         
     def seven(self):
         if len(self.label_input.text) > 7: pass
         elif self.label_input.text == '':
             self.label_input.text = '7'
         else: self.label_input.text += '7'
-
-        game.label_input = self.label_input.text
+        
         
     def eight(self):
         if len(self.label_input.text) > 7: pass
         elif self.label_input.text == '':
             self.label_input.text = '8'
         else: self.label_input.text += '8'
-
-        game.label_input = self.label_input.text
+        
     
     def nine(self):
         if len(self.label_input.text) > 7: pass
         elif self.label_input.text == '':
             self.label_input.text = '9'
         else: self.label_input.text += '9'
-
-        game.label_input = self.label_input.text
+        
         
     def zero(self):
         if len(self.label_input.text) > 7: pass
         elif self.label_input.text == '':
             self.label_input.text = '0'
         else: self.label_input.text += '0'
-
-        game.label_input = self.label_input.text
+        
         
     def delete(self):
         if self.label_input.text != '':
             self.label_input.text = self.label_input.text[:-1]
-
-        game.label_input = self.label_input.text
+        
 
     def submit(self):
         game.printr()
